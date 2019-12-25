77fbd3f7ef737bdee3282e94a688768f42fdc9e2
hathienvan58770769 <43336231+hathienvan58770769@users.noreply.github.com>
2019-12-25 10:46:53 +0700

    update

---
+++ LoginEmail/LoginEmail/ViewControllers/MessagerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MessagerViewController.swift
+//  LoginEmail
+//
+//  Created by Van on 12/25/19.
+//  Copyright © 2019 Van. All rights reserved.
+//
+
+import UIKit
+
+class MessagerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
